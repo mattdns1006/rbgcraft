aa321b23158064981ba554c61fe0fd1aeb7e9fe9
0xEntropie <75908345+0xEntropie@users.noreply.github.com>
2023-05-09 10:16:12 +0200

    updated config file with PC resolution and correct binding number

--- fishing/config.py
+++ fishing/config.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 SPEAKER_ID = None  # speaker to listen to for fish sound
 SOUND_THRESH = 0.002  # sound threshold for catching fish.
-OUTPUT_FOLDER = Path(r"C:\temp\rgbcraft")  # where to save outputs (images/audio plots) for debugging
-PIX_X, PIX_Y = 2560, 1080  # size of screen in pixels
+OUTPUT_FOLDER = Path(r"D:\00_DEV\00.1-rgbcraft-1\temp")  # where to save outputs (images/audio plots) for debugging
+PIX_X, PIX_Y = 1920, 1080  # size of screen in pixels
 SEC = 1
 SAMPLE_RATE = 48000  # audio sample rate
 WAIT_PARAMETER = 2  # parameter passed to exponential distribution to sample wait time in seconds
